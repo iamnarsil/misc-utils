79356fc8e749cb99745e3932a01934d92066e053
iamnarsil <arpu1011@gmail.com>
2023-07-10 16:39:25 +0800

    [20230710] - add 'read/write text' function to file operation

--- src/main/java/com/narsil/misc/FileUtils.java
+++ src/main/java/com/narsil/misc/FileUtils.java
@@ -1,16 +1,23 @@
 package com.narsil.misc;
 
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Logger;
+import java.util.stream.Stream;
 
 /**
  * file operation toolkit
  *
  * @author iamnarsil
- * @version 20230705
+ * @version 20230710
  * @since 20230328
  */
 public class FileUtils {
@@ -20,9 +27,9 @@ public class FileUtils {
     /**
      * write raw data to file
      *
-     * @param dirPath directory path
+     * @param dirPath  directory path
      * @param fileName file name
-     * @param bytes raw data
+     * @param bytes    raw data
      * @return result
      */
     public static boolean writeBytesToPath(String dirPath, String fileName, byte[] bytes) {
@@ -53,9 +60,49 @@ public class FileUtils {
     }
 
     /**
+     * write text data to file
+     *
+     * @param dirPath  directory path
+     * @param fileName file name
+     * @param textList text data
+     * @return result
+     */
+    public static boolean writeTextToPath(String dirPath, String fileName, List<String> textList) {
+
+        if (dirPath == null || dirPath.isBlank() || fileName == null || fileName.isBlank() || textList == null || textList.isEmpty()) {
+            return false;
+        }
+
+        if (dirPath.endsWith(File.separator)) {
+            dirPath = dirPath.substring(0, dirPath.length() - 1);
+        }
+
+        File dir = new File(dirPath);
+        if (!dir.exists() && !dir.mkdirs()) {
+            return false;
+        }
+
+        String filePath = dirPath.concat(File.separator).concat(fileName);
+
+        String text = "";
+        for (String s : textList) {
+            text = text.concat(s).concat("\n");
+        }
+
+        try (FileWriter fileWriter = new FileWriter(filePath); BufferedWriter bufferedWriter = new BufferedWriter(fileWriter)) {
+            bufferedWriter.write(text);
+        } catch (Exception e) {
+            // e.printStackTrace();
+            LOGGER.severe(e.getMessage());
+        }
+
+        return true;
+    }
+
+    /**
      * read raw data from file
      *
-     * @param dirPath directory path
+     * @param dirPath  directory path
      * @param fileName file name
      * @return raw data
      */
@@ -90,6 +137,44 @@ public class FileUtils {
         }
 
         return bytes;
+    }
+
+    /**
+     * read text data from file
+     *
+     * @param dirPath directory path
+     * @param fileName file name
+     * @return text data
+     */
+    public static List<String> readTextFromPath(String dirPath, String fileName) {
+
+        List<String> textList = new ArrayList<>();
+
+        if (dirPath == null || dirPath.isBlank() || fileName == null || fileName.isBlank()) {
+            return textList;
+        }
+
+        if (dirPath.endsWith(File.separator)) {
+            dirPath = dirPath.substring(0, dirPath.length() - 1);
+        }
+
+        String filePath = dirPath.concat(File.separator).concat(fileName);
+        File file = new File(filePath);
+
+        if (!file.exists()) {
+            return textList;
+        }
+
+        // use stream -> efficiently reading large file
+        // NOTE: the file is processed lazily -> only part of the content is stored in memory at a given time
+        try (Stream<String> lines = Files.lines(Paths.get(filePath))) {
+            lines.forEach(textList::add);
+        } catch (Exception e) {
+            // e.printStackTrace();
+            LOGGER.severe(e.getMessage());
+        }
+
+        return textList;
     }
 
     /**
--- src/test/java/com/narsil/misc/FileUtilsTest.java
+++ src/test/java/com/narsil/misc/FileUtilsTest.java
@@ -6,7 +6,9 @@ import org.junit.Test;
 import org.junit.runners.MethodSorters;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.Base64;
+import java.util.List;
 import java.util.logging.Logger;
 
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
@@ -16,15 +18,23 @@ public class FileUtilsTest {
 
     private String dirPath;
     private String binaryFileName;
+    private String textFileName;
     private String jsonFileName;
     private byte[] raw;
+    private List<String> textList;
 
     @Before
     public void init() {
         dirPath = "D:\\temp";
         binaryFileName = "test.bin";
+        textFileName = "test.txt";
         jsonFileName = "test.json";
         raw = Base64.getDecoder().decode("YCJfAQQwMTA4XzYGMDQwMDAwXBBhdWN2ZWZnaGlqa2xtbm9w");
+
+        textList = new ArrayList<>();
+        textList.add("{\"alphaCode\":9999,\"betaMessage\":\"this is a message\", \"gammaDate\":\"2023-06-15 06:05:11.143\"}");
+        textList.add("YCJfAQQwMTA4XzYGMDQwMDAwXBBhdWN2ZWZnaGlqa2xtbm9w");
+        textList.add("0123465789");
     }
 
     @Test
@@ -34,13 +44,25 @@ public class FileUtilsTest {
     }
 
     @Test
-    public void test01_readBytesFromPath() {
-        byte[] bytes = FileUtils.readBytesFromPath(dirPath, binaryFileName);
-        MiscUtils.showBytes(bytes, binaryFileName);
+    public void test01_writeTextToPath() {
+        boolean result = FileUtils.writeTextToPath(dirPath, textFileName, textList);
+        LOGGER.info("result = " + result);
     }
 
     @Test
-    public void test02_getFileFromClassPath() {
+    public void test02_readBytesFromPath() {
+        byte[] bytes = FileUtils.readBytesFromPath(dirPath, binaryFileName);
+        MiscUtils.showBytes(bytes, textFileName);
+    }
+
+     @Test
+    public void test03_readTextFromPath() {
+        List<String> textList = FileUtils.readTextFromPath(dirPath, textFileName);
+        MiscUtils.showList(textList, textFileName);
+    }
+
+    @Test
+    public void test04_getFileFromClassPath() {
         File file = FileUtils.getFileFromClassPath(jsonFileName);
         if (file != null) {
             LOGGER.info("file path = " + file.getAbsolutePath());
